67a2ba350503e643448f4c2f5376d569288ac17e
Suyeol Jeon <devxoul@gmail.com>
2017-05-31 01:27:21 +0900

    Use RxExpect dependency only when TEST=1

--- Package.swift
+++ Package.swift
@@ -1,11 +1,22 @@
 // swift-tools-version:3.1
 
+import Foundation
 import PackageDescription
 
-let package = Package(
-  name: "ReactorKit",
-  dependencies: [
+let isTest = ProcessInfo.processInfo.environment["TEST"] == "1"
+let dependencies: [Target.Dependency]
+if !isTest {
+  dependencies = [
+    .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3),
+  ]
+} else {
+  dependencies = [
     .Package(url: "https://github.com/devxoul/RxSwift.git", majorVersion: 3),
     .Package(url: "https://github.com/devxoul/RxExpect.git", majorVersion: 0),
   ]
+}
+
+let package = Package(
+  name: "ReactorKit",
+  dependencies: dependencies
 )
